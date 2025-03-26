49e6b6c19692e39bdf9ac7eeba5cf22ead818d8e
Oussama-Kahlaoui <ousama.kahlaoui@gmail.com>
2025-03-26 22:39:45 +0000

    Add Logo Styled Icon Pgae

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,18 +1,31 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
+import { Link, useLocation } from 'react-router-dom';
 import '../styles/Navbar.css';
+import logo from '../assets/logo.png';
 
 const Navbar = () => {
+  const location = useLocation();
+
   return (
     <nav className="navbar">
       <div className="navbar-brand">
-        <Link to="/">Blood Bank</Link>
+        <Link to="/">
+          <img src={logo} alt="Blood Bank Logo" className="navbar-logo" />
+        </Link>
       </div>
       <div className="navbar-links">
-        <Link to="/">Home</Link>
-        <Link to="/donate">Donate</Link>
-        <Link to="/request">Request Blood</Link>
-        <Link to="/dashboard">Dashboard</Link>
+        <Link to="/" className={location.pathname === '/' ? 'active' : ''}>
+          <i className="fas fa-home"></i> Home
+        </Link>
+        <Link to="/donate" className={location.pathname === '/donate' ? 'active' : ''}>
+          <i className="fas fa-hand-holding-heart"></i> Donate
+        </Link>
+        <Link to="/request" className={location.pathname === '/request' ? 'active' : ''}>
+          <i className="fas fa-hand-holding-medical"></i> Request Blood
+        </Link>
+        <Link to="/dashboard" className={location.pathname === '/dashboard' ? 'active' : ''}>
+          <i className="fas fa-chart-line"></i> Dashboard
+        </Link>
       </div>
     </nav>
   );
